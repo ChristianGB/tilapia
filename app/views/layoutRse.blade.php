c23e369f7291a831052632628ffcf4c339bfbbf1
leyven <nyhedgg@gmail.com>
2014-08-14 14:08:51 -0500

    i hope saw
    saw :dancer:

---
+++ app/views/layoutRse.blade.php
@@ -0,0 +1,15 @@
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<div id="Programas_rse">   		 @yield('Programas_rse')</div>
+<div id="Eventos">     @yield('Eventos')</div>
+<div id="Certificaciones">  		 @yield('Certificaciones')</div>
+<div id="Politica_Amb">    @yield('Politica_Amb')</div>
+
+
+</body>
+</html>
---
+++ app/views/rse.blade.php
@@ -0,0 +1,17 @@
+@extends('layoutRse')
+
+@section('Programas_rse')
+  <h3>Programas RSE</h3>
+@stop
+
+@section('Eventos')
+  <h3>Eventos</h3>
+@stop
+
+@section('Certificaciones')
+  <h3>Certificaciones</h3>
+@stop
+
+@section('Politica_Amb')
+  <h3>Politica Ambiental</h3>
+@stop
