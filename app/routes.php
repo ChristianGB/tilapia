e04b9a1edc2dc84afb653e85cefd25c11693b9b8
leyven <nyhedgg@gmail.com>
2014-08-14 13:44:23 -0500

    ad
    asdf

--- app/routes.php
+++ app/routes.php
@@ -5,3 +5,4 @@ Route::get('/', 'InicioController@mostrarNoticia');
 Route::get('/bolsa-de-trabajo', 'InicioController@mostrarBolsa');
 
 Route::get('/nosotros', 'NosotrosController@mostrarNosotros');
+Route::get('productos','ProductoController@getProductos');
