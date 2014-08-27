3f02e096a30ef56553d0e94c1b285494b4b0593f
Alexis <leonidas101@live.com.mx>
2014-08-27 12:55:13 -0500

    cambios2 3er semana
    jejejjee

--- app/views/formulario.blade.php
+++ app/views/formulario.blade.php
@@ -28,25 +28,20 @@
 <div id:"puestos">
 <h2>Formulario de puestos</h2>
 
-{{Form::open(array('action' => 'FormularioController@formularioNoticia'))}}
+{{Form::open(array('action' => 'FormularioController@formularioPuesto'))}}
 <br>
-	{{Form::label('Titulo', 'Escribe el titulo de la noticia: ')}}
+	{{Form::label('Titulo', 'Escribe el titulo de: ')}}
 	{{Form::text('Titulo')}}
 </br>
 
-	{{Form::label('Descripcion', 'Escribe la descripcion de la noticia: ')}}<br>
+	{{Form::label('Descripcion', 'Escribe la descripcion del puesto: ')}}<br>
 	{{Form::textarea('Descripcion')}}
-<br>
-
-	{{Form::label('Imagen', 'Escribe la url de la imagen de la noticia: ')}}
-	{{Form::text('Imagen')}}
-	{{Form::selectMonth('month')}}
-<br>
-	{{Form::select('animal', array('Cats' => array('leopard' => 'Leopard'),'Dogs' => array('spaniel' => 'Spaniel')))}}
-
 <br>
 	{{Form::submit('Guardar')}}
 {{Form::close()}}
 
 <br>
 </div>
+
+
+
