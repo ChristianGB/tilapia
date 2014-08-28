ccff6cd530341e01576cb17e281c6c2f9c564db8
leyven <nyhedgg@gmail.com>
2014-08-28 15:15:15 -0500

    added login and all forms
    forms need a godamm menu

---
+++ app/models/Usuarios.php
@@ -0,0 +1,37 @@
+<?php
+use Illuminate\Auth\UserInterface;
+
+Class Usuarios extends Eloquent implements UserInterface{
+ 
+    protected $table = 'usuarios';
+    protected $fillable = array('nombre', 'correo', 'password');
+ 
+    // este metodo se debe implementar por la interfaz
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+    
+    //este metodo se debe implementar por la interfaz
+    // y sirve para obtener la clave al momento de validar el inicio de sesión 
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberToken()
+{
+    return $this->remember_token;
+}
+
+public function setRememberToken($value)
+{
+    $this->remember_token = $value;
+}
+
+public function getRememberTokenName()
+{
+    return 'remember_token';
+}
+    
+}
